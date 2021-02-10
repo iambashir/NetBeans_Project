eb9c80ce94fa8b5eb6777ff42efba9a7b5801765
iambashir <bashir.rasulpur@gmail.com>
2021-02-10 13:08:29 +0600

    1.08

--- ClassProject/src/classproject/P01a2_Fibonacci.java
+++ ClassProject/src/classproject/P01a2_Fibonacci.java
@@ -1,27 +1,28 @@
 package classproject;
 import java.util.Scanner;
 public class P01a2_Fibonacci {
+
  /** Main method */
  public static void main(String[] args) {
+     
 // Create a Scanner
- Scanner input = new Scanner(System.in);
- System.out.print("Enter an index for a Fibonacci number: ");
- int index = input.nextInt();
+ Scanner scan = new Scanner(System.in);
+ System.out.print("Enter value to check Fabonacci number: ");
+ int n = scan.nextInt();
 
  // Find and display the Fibonacci number
- System.out.println("The Fibonacci number at index "
- + index + " is " + fib(index));
+ System.out.println("The Fibonacci number of input value is: " + fib(n));
  }
 
  /** The method for finding the Fibonacci number */
- public static long fib(long index) {
- if (index == 0) // Base case
+ public static long fib(long n) {
+ if (n == 0) // Base case
  return 0;
 
- else if (index == 1) // Base case
+ else if (n == 1) // Base case
  return 1;
  else // Reduction and recursive calls
- return fib(index - 1) + fib(index - 2);
+ return fib(n - 1) + fib(n - 2);
 
 }
 }
