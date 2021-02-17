0c285a242c62725547a2b2546d16d42eb1823e79
iambashir <bashir.rasulpur@gmail.com>
2021-02-17 12:35:13 +0600

    17.02.21

---
+++ ClassProject/src/classproject/P37_Time.java
@@ -0,0 +1,20 @@
+package classproject;
+
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime; 
+
+public class P37_Time {
+
+  public static void main(String[] args) {    
+   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+   LocalDateTime now = LocalDateTime.now();  
+   System.out.println(dtf.format(now)); 
+   
+   
+   
+   
+  }    
+} 
+
+
+
